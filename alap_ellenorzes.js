6b00ab9c46a47fc6363a2f8eb293c65b2668f1f6
Barna Csaba <barna.csaba@diak.szbi-pg.hu>
2025-12-12 08:46:12 +0100

    a

--- alap_ellenorzes.js
+++ alap_ellenorzes.js
@@ -1,1 +1,26 @@
 // /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/
+
+function ellenorAlap(){
+    let nev = document.forms["alapUrlap"]["nev"].value;
+    console.log(nev);
+
+
+if(nev.trim() === ""){
+    alert("A név mező nem lehet üres!");
+    if(nevKiiratasa){
+        nevKiiratasa.innerText = "A mező nem lehet üres!";
+    }
+    return false;
+}
+
+let nevFilter = /^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ ]+$/;
+if(!nevFilter.test(nev)){
+    alert("A név csak betűket és szóközöket tartalmazhat!");
+    return false;
+}
+
+let nevKiiratasa = document.getElementById("nevEredmeny");
+nevKiiratasa.innerText = "A megadott név: " + nev;;
+
+return false;
+}
