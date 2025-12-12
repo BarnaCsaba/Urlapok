90dbf661cd6e7500cfd56af3ffbe03edcab511b5
Barna Csaba <barna.csaba@diak.szbi-pg.hu>
2025-12-12 09:51:46 +0100

    b

--- telefonalo_email_ellenorzes.js
+++ telefonalo_email_ellenorzes.js
@@ -12,5 +12,22 @@ function telefonError(telefon) {
     if (!filter.test(newString)) {
         error = "Érvénytelen telefonszám! Csak számok használhatók.";
     }
-    return false;
+    return error;
+}
+
+
+function ellenor(){
+    let telefon = document.forms["urlap"]["tel"].value;
+    let email = document.forms["urlap"]["mail"].value;
+
+    let telError = telefonError(telefon);
+    if(telError){
+        alert(telError);
+        return false;
+    }
+    else{
+        let telefonEredmeny = document.getElementById("telEredmeny");
+        telefonEredmeny.innerText = "A megadott telefonszám érvényes: " + telefon;
+        return false;
+    }
 }
